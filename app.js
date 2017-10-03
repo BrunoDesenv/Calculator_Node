9686a618ec8f139f6de9935aa11fee8f01be908a
Bruno Santos <brunoj.santos91@gmail.com>
2017-10-03 00:00:16 -0300

    Create project

---
+++ app.js
@@ -0,0 +1,13 @@
+var calculator = require('./calculator');
+
+var resultAdd = calculator.add(1, 2);
+var resultSubtract= calculator.subtract(3, 2);
+var resultMultiply = calculator.multiply(2, 2);
+var resultDivide= calculator.divide(1, 2);
+
+
+console.log(resultAdd);
+console.log(resultSubtract);
+console.log(resultMultiply);
+console.log(resultDivide);
+
